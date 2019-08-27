b1c3c94457cfa7109ec4ff1c9cd3d76d5e4280f7
Izabela Furdzik <izabela.furdzik@pracuj.pl>
2019-08-27 15:54:30 +0200

    feat(app): create routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { IntlProvider, addLocaleData } from 'react-intl';
+import { defaultLocale } from './lang/config';
+import { polish } from './lang';
+
+import { BrowserRouter as Router, Route, Link  } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Router>
+        <IntlProvider
+          locale={defaultLocale}
+          messages={polish}
         >
-          Learn React
-        </a>
-      </header>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/kanji/">Kanji</Link>
+              </li>
+            </ul>
+          </nav>
+          <Route path="/" exact component={Index} />
+          <Route path="/kanji/" component={Kanji} />
+        </IntlProvider>
+      </Router>
     </div>
   );
+}
+
+function Index() {
+  return <h2>Home</h2>;
+}
+
+function Kanji() {
+  return <h2>Kanji</h2>;
 }
 
 export default App;
---
+++ src/lang/index.js
@@ -0,0 +1,4 @@
+import componentsPl from './components/pl.json';
+import defaultsPl from './defaults/pl.json';
+
+export const polish = { ...componentsPl, ...defaultsPl };
