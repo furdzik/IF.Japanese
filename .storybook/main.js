240cfa7f37c6b40553cb01b1c6461a6835451280
i.furdzik <izabela.furdzik@pracuj.pl>
2022-02-08 08:19:06 +0100

    update(app): update linter packages and package.json

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,46 +3,70 @@ const toPath = (_path) => path.join(process.cwd(), _path);
 
 const dotenv = require('dotenv');
 const {DefinePlugin} = require('webpack');
+const {resolve} = require('path');
 
 module.exports = {
-  stories: [
-    "../src/components/**/*.stories.mdx",
-    "../src/components/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  core: {
+  "features": {
+    "storyStoreV7": true,
+    "babelModeV7": true
+  },
+  "core": {
     builder: 'webpack5',
   },
-  addons: [
-    "@storybook/addon-links",
-    '@storybook/addon-essentials',
-    "@storybook/theming"
+  framework: '@storybook/react',
+  "stories": [
+    "../src/components/**/*.stories.mdx",
+    "../src/components/**/*.stories.jsx",
+    "../src/components/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  webpackFinal: async (config) => {
+  "addons": [
+    "@storybook/addon-essentials",
+    "@storybook/addon-viewport",
+    "@storybook/addon-a11y",
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-postcss"
+  ],
+  webpackFinal: async function (config) {
     const envVariables = dotenv.config().parsed;
 
-    return {
-      ...config,
-      resolve: {
-        ...config.resolve,
-        alias: {
-          ...config.resolve.alias,
-          "@emotion/core": path.resolve("node_modules/@emotion/react"),
-          "@emotion/styled": path.resolve("node_modules/@emotion/styled"),
-          "emotion-theming": path.resolve("node_modules/@emotion/react"),
-          "@emotion/react": path.resolve("node_modules/@emotion/react"),
-        }
-      },
-      plugins: [
-        ...config.plugins,
-        new DefinePlugin({
-          NODE_ENV: JSON.stringify(envVariables.NODE_ENV),
-          APP_URL: JSON.stringify(envVariables.APP_URL),
-          KANJIALIVE_API_KEY: JSON.stringify(envVariables.KANJIALIVE_API_KEY),
-          JISHO_API_URL: JSON.stringify(envVariables.JISHO_API_URL),
-          KANJIALIVE_API_URL: JSON.stringify(envVariables.KANJIALIVE_API_URL),
-          ALTERNATIVE_KANJI_API_URL: JSON.stringify(envVariables.ALTERNATIVE_KANJI_API_URL)
-        })
-      ]
+    config.resolve = {
+      ...config.resolve,
+      extensions: ['.js', '.jsx', '.json'],
+      mainFields: ['browser', 'main', 'module'],
+      modules: [resolve('./'), resolve('node_modules')]
     };
+
+    config.module.rules.push({
+      test: /\.(scss|css)$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+    });
+
+    config.module.rules.push({
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      options: {
+        plugins: [
+          ["babel-plugin-styled-components", {
+            pure: false,
+            displayName: true
+          }]
+        ]
+      }
+    });
+
+    config.module.plugins.push(
+      new DefinePlugin({
+        NODE_ENV: JSON.stringify(envVariables.NODE_ENV),
+        APP_URL: JSON.stringify(envVariables.APP_URL),
+        KANJIALIVE_API_KEY: JSON.stringify(envVariables.KANJIALIVE_API_KEY),
+        JISHO_API_URL: JSON.stringify(envVariables.JISHO_API_URL),
+        KANJIALIVE_API_URL: JSON.stringify(envVariables.KANJIALIVE_API_URL),
+        ALTERNATIVE_KANJI_API_URL: JSON.stringify(envVariables.ALTERNATIVE_KANJI_API_URL)
+      })
+    );
+
+    return config;
   }
 };
