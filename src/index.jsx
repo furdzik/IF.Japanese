ae0a954cf9af417dbf6d3e07405ba988582f4c7e
i.furdzik <izabela.furdzik@pracuj.pl>
2022-04-26 22:10:13 +0200

    update(app): update emotion

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,15 @@
 import '@utils/polyfills';
 
-import React from 'react';
-import { render } from 'react-dom';
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
 import App from '@src/App';
 
-render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
