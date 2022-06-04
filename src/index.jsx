b5f9c80387aeef8c41d5ec9989fd4b8d88e58361
Izabela <izabela.furdzik@gmail.com>
2022-06-04 21:07:27 +0200

    feat(app): #121 - add sentry

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,23 @@ import '@utils/polyfills';
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
+
 import App from '@src/App';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
+
+Sentry.init({
+  dsn: 'https://63c96e2afb0a417c83afd786d4071ba6@o1270468.ingest.sentry.io/6461319',
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0
+});
 
 root.render(
   <StrictMode>
